418d99854ce93e62a7d10acc11182b9de2fb1032
Christian Zamudio <cz584688@dcd153.ecs.csun.edu>
2019-03-07 23:45:58 -0800

    Had to update non verification module name

--- lab1_b/lab1b.sv
+++ lab1_b/lab1b.sv
@@ -1,6 +1,7 @@
 `timescale 1ns/1ns
-module lab1b( CLK, N_RST_1,N_RST_2, EN_1, EN_2, UP_DWN1, UP_DWN2, COUNT1, COUNT2);
-//declare parameters for two instances
+module lab1b( CLK, N_RST_1,N_RST_2, EN_1, EN_2,  UP_DWN1, UP_DWN2, COUNT1, COUNT2);
+ 
+ //declare parameters for two instances
  parameter COUNTER1_SIZE  = 16;
  parameter COUNTER2_SIZE = 32;
 
@@ -15,14 +16,20 @@ module lab1b( CLK, N_RST_1,N_RST_2, EN_1, EN_2, UP_DWN1, UP_DWN2, COUNT1, COUNT2
 
  input UP_DWN1;
  input UP_DWN2;
+ 
+ //input LD1;
+ //input LD2;
+
+ //input [COUNTER1_SIZE -1:0] VALUE1;
+ //input [COUNTER2_SIZE-1:0] VALUE2;
 
  //Declare outputs
  output reg [COUNTER1_SIZE - 1 : 0] COUNT1;
  output reg [COUNTER2_SIZE - 1 : 0] COUNT2;
 
  //Instantiate two counter modules
- UP_DOWN_COUNTER #(COUNTER1_SIZE) count1 (.CLK(CLK), .N_RST(N_RST_1), .EN(EN_1), .UP_DWN(UP_DWN1), .COUNT(COUNT1));
- UP_DOWN_COUNTER #(COUNTER2_SIZE) count2 (.CLK(CLK), .N_RST(N_RST_2), .EN(EN_2), .UP_DWN(UP_DWN2), .COUNT(COUNT2));
+ UpdownCounter #(COUNTER1_SIZE) count1 (.CLK(CLK), .N_RST(N_RST_1), .EN(EN_1), .UP_DWN(UP_DWN1), .COUNT(COUNT1));
+ UpdownCounter #(COUNTER2_SIZE) count2 (.CLK(CLK), .N_RST(N_RST_2),.EN(EN_2), .UP_DWN(UP_DWN2), .COUNT(COUNT2));
 
 
 endmodule
